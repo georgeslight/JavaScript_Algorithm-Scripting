6b50aeea03b09f5c4c04a96ab79c80cb5e481051
George Slight <georgeslight@gmail.com>
2022-08-30 11:27:19 +0200

    Chunky Monkey

---
+++ Basic Algorithm Scripting/chunkyMonkey.js
@@ -0,0 +1,17 @@
+// Write a function that splits an array (first argument) into groups the length of
+// size (second argument) and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+  if (arr.length <= size) {
+    return [arr];
+  }
+  return [arr.slice(0, size)].concat(chunkArrayInGroups(arr.slice(size), size));
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2)); // [["a", "b"], ["c", "d"]]
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3)); // [[0, 1, 2], [3, 4, 5]]
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2)); // [[0, 1], [2, 3], [4, 5]]
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) ); // [[0, 1, 2, 3], [4, 5]]
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)); // [[0, 1, 2], [3, 4, 5], [6]]
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)); // [[0, 1, 2, 3], [4, 5, 6, 7], [8]]
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)); // [[0, 1], [2, 3], [4, 5], [6, 7], [8]]
